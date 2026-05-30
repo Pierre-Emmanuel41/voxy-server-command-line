21ba3fa2e61b5a2d1a64b1bc4a886bf0f9660a06
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2026-05-30 11:58:51 +0200

    Logger class has static methods only

--- src/main/java/fr/pederobien/voxy/server/commandline/VoxyServerCLIApp.java
+++ src/main/java/fr/pederobien/voxy/server/commandline/VoxyServerCLIApp.java
@@ -6,11 +6,19 @@ import fr.pederobien.utils.event.Logger;
 public class VoxyServerCLIApp {
 
 	public static void main(String[] args) {
-		Logger logger = Logger.instance();
-		logger.colorized(true);
+		Logger.setPrintInColor(true);
+		Logger.setPrintEvent(true);
 
-		if (args.length > 0 && args[0].equals("-d"))
-			logger.debug(true);
+		if (args.length == 0)
+			// Max debug level is 3 in project voxy-server
+			Logger.setPrintDebugLevel(4);
+		if (args.length >= 2 && args[0].equals("-d"))
+			try {
+				Logger.setPrintDebugLevel(Integer.parseInt(args[1]));
+			} catch (Exception e) {
+				Logger.warning("Minimum debug level could not be parsed");
+				Logger.setPrintDebugLevel(4);
+			}
 
 		Runnable cli = CLI.simpleInterface("voxy>", arg -> arg.equals("exit"), new VoxyCommandTree().getTree());
 		cli.run();

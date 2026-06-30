66cefff8fbe188ba5d23a4b627bac0cb308e5af1
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2026-06-30 22:29:29 +0200

    Adding parseArgs method to dynamically set debug level and events display

--- src/main/java/fr/pederobien/voxy/server/commandline/VoxyServerCLIApp.java
+++ src/main/java/fr/pederobien/voxy/server/commandline/VoxyServerCLIApp.java
@@ -7,20 +7,41 @@ public class VoxyServerCLIApp {
 
 	public static void main(String[] args) {
 		Logger.setPrintInColor(true);
-		Logger.setPrintEvent(true);
-
-		if (args.length == 0)
-			// Max debug level is 3 in project voxy-server
-			Logger.setPrintDebugLevel(4);
-		if (args.length >= 2 && args[0].equals("-d"))
-			try {
-				Logger.setPrintDebugLevel(Integer.parseInt(args[1]));
-			} catch (Exception e) {
-				Logger.warning("Minimum debug level could not be parsed");
-				Logger.setPrintDebugLevel(4);
-			}
+		parseArgs(args);
 
 		Runnable cli = CLI.simpleInterface("voxy>", arg -> arg.equals("exit"), new VoxyCommandTree().getTree());
 		cli.run();
+	}
+
+	private static void parseArgs(String[] args) {
+		if (args.length == 0) {
+			Logger.setPrintDebugLevel(Integer.MAX_VALUE);
+			return;
+		}
+
+		for (int i = 0; i < args.length; /* Managed inside the loop */) {
+			// Debug
+			if (args[i].equals("-d")) {
+				i++;
+				try {
+					int level = Integer.parseInt(args[i]);
+					Logger.setPrintDebugLevel(level);
+					Logger.info("Minimum debug level is %s", level);
+					i++;
+				} catch (Exception e) {
+					Logger.warning("Minimum debug level could not be parsed, setting to maximum value (ie: no debug output)");
+					Logger.setPrintDebugLevel(Integer.MAX_VALUE);
+				}
+
+				continue;
+			}
+
+			// Event
+			if (args[i].equals("-e")) {
+				Logger.setPrintEvent(true);
+				Logger.info("Internal events will be printed");
+				i++;
+			}
+		}
 	}
 }
